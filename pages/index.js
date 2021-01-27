c12ba038231e44420a5efdcf1a59156c46fa9a6a
sidney_silva_verity <jhonyxf@gmail.com>
2021-01-27 12:17:51 -0300

    Insert button input logo

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-/*
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex:1;
-  background-size: cover;
-  background-position: center;
-`;
-*/
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
-
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
@@ -26,29 +30,52 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
-        <Widget.Header>
-            <h1>The legends of zelda</h1>
+          <Widget.Header>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem impsun daiodjfa doifjaof</p>
-         </Widget.Content>
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
+          </Widget.Content>
         </Widget>
+
         <Widget>
           <Widget.Content>
-          <h1>The legends of zelda</h1>
-          <p>Lorem impsun daiodjfa doifjaof</p>
-         </Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/jhonyxf" />
-      </QuizBackground>
+    </QuizBackground>
   );
 }
